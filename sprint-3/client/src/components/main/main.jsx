74f04c2917b1e205646b5c31ea80fcac6df77f2f
mollybeach <56612434+mollybeach@users.noreply.github.com>
2021-03-14 14:51:26 -0700

    cleaned up code to be easier to read

--- sprint-3/client/src/components/main/main.jsx
+++ sprint-3/client/src/components/main/main.jsx
@@ -1,4 +1,4 @@
-/****************IMPORTS***************/
+/***********************IMPORTS***************/
 import React from "react";
 import axios from "axios";
 import HeroVideo from "../herovideo/herovideo";
@@ -10,10 +10,10 @@ let API_URL = "http://localhost:8080/videos/";
 let defaultVideoId ="1af0jruup5gu";
 let paramsId;
 class Main extends React.Component {
-/****************STATE**************/
+/***********************STATE********************/
   state = {
     topVideo: {},
-    sideVideos: [],
+    subData: [],
   };
 /*******************COMPONENT METHODS**************/
 
@@ -32,20 +32,20 @@ componentWillUnmount() {
       this.updateInformation();
     } 
   }
-  /****************GET DATA FROM BACKEND************/
+  /********************GET DATA FROM BACKEND************/
   getInformation = () => {
     axios.get(`${API_URL}`)
     .then((response) => {
-      const sideVideos = response.data;
-      this.setState({ sideVideos });
+      let subData = response.data;
+      this.setState({ subData });
       if(defaultVideoId !== this.props.match.params.id) {
         if(this.props.match.params.id === undefined) {
         this.props.match.params.id = defaultVideoId
         }
         paramsId = this.props.match.params.id;
       axios.get(`${API_URL}${paramsId}`)
-        .then((topVideoResponse) => {
-          const topVideo = topVideoResponse.data;
+        .then((response) => {
+          let topVideo = response.data;
           this.setState({ topVideo });
         })
         .catch((err) => {
@@ -70,7 +70,6 @@ updateInformation = () => {
           console.log(err);
         });
       }
-
 /*************************RENDER********************/
 
 renderComments = () =>{
@@ -85,7 +84,7 @@ renderComments = () =>{
             <About aboutData={this.state.topVideo}/>
             <Feed commentData={this.state.topVideo} renderComments={this.renderComments} topVideo={this.state.topVideo}/>
           </div>
-          <SubVideo topData={this.state.topVideo} match={this.props.match}  defaultId={this.defaultId}  data={this.state.sideVideos} videoData={this.state.topVideo} />
+          <SubVideo topData={this.state.topVideo} match={this.props.match}  defaultId={this.defaultId}  subData={this.state.subData}  />
         </div>
       </div>
     );
@@ -93,46 +92,3 @@ renderComments = () =>{
 }
 export default Main;
 
-  /****************STATE**************/
- 
-  
-/****************GET DATA FROM BACKEND************/
-/*
- *  state = {
-    topVideo: [],
-    commentData: []
-  };
-  componentIsMounted = false;
-  getInformation = (id) => {
-    console.log( `${API_URL}${id}`);
-          axios.get(`${API_URL}${id}`)
-          .then((response) => {
-            this.setState({ 
-              topVideo:response.data,
-              commentData: response.data.comments
-            });
-            
-          })
-          .catch((error) => {
-            console.log(error);
-          });
-        
-        }
-
-  renderComments = () =>{
-    this.getInformation(this.defaultId)
-}
-componentDidMount() {
-   this.componentIsMounted = true;
-   this.getInformation(this.defaultId);
-}
-/*
-componentDidUpdate() {
-  let videoid = this.props.match.params.videoid;
-    if(this.state.topVideo !== undefined){
-        if(this.defaultId !== this.state.topVideo.id){this.getInformation(this.props.defaultId)}
-    }else{
-        this.getInformation(this.defaultId);
-    }
-}
-*/
--- sprint-3/client/src/components/subvideo/subvideo.jsx
+++ sprint-3/client/src/components/subvideo/subvideo.jsx
@@ -1,52 +1,24 @@
-
+/********************IMPORTS*******************/
 import React from 'react';
 import './subvideo.scss';
-import axios from 'axios';
 import {Link} from 'react-router-dom';
-
-//const API_URL = process.env.REACT_APP_API_URL;
-const API_URL = "http://localhost:8080/videos/";
-
-class SubVideo extends React.Component {
-    state = {
-        subVideoData : []
-    }
-
-    componentDidMount () {
-        this.getSubVideoData()
-    }
-
-    getSubVideoData = () => {
-        axios.get (`${API_URL}`)
-        .then (response => {
-            this.setState({
-                subVideoData: response.data
-            })
-        })
-    }
-
-    render () {
-        let newSubVideoData = this.state.subVideoData.filter((item) => item.id !== this.props.topData.id);
-        return(
-            <section className="subvideo">
-            <div className="subvideo__title">NEXT VIDEO</div>
-                { newSubVideoData.map( (props) => {
-                    //to={`/video/${video.id}`}
-        return (
-                    <Link to={`/videos/${props.id}`} className="subvideo__link subvideo__partition"  key={ props.id} id={props.id}>
-                        <video className="subvideo__partition-film" poster={ props.image} ></video>
-                        <div className="subvideo__partition-note">
-                        <div className="subvideo__partition-title">{ props.title}</div>
-                        <div className="subvideo__partition-channel">{ props.channel}</div>
-                        </div>
-                    </Link>
-                    )
-                })
-            }
-            </section>
-        )
-    }
+/*******************SUBVIDEO******************/
+export default function SubVideo({ subData, topData }) {
+    let updatedVideo =  subData.map((sideData) => {
+      if (topData.id !== sideData.id) {
+        return ( <Link  to={`/videos/${sideData.id}`} className="subvideo__link subvideo__partition"  key={ sideData.id} id={sideData.id}> 
+             <video className="subvideo__partition-film" poster={sideData.image} ></video>
+              <div className="subvideo__partition-note">
+              <div className="subvideo__partition-title">{ sideData.title}</div>
+              <div className="subvideo__partition-channel">{ sideData.channel}</div>
+              </div>
+              </Link>
+        );
+      }
+    });
+    return( <section className="subvideo">
+    <div className="subvideo__title">NEXT VIDEO</div>
+    {updatedVideo}  
+    </section>
+    );
 }
-
-export default SubVideo;
-//to={`/${props.id}`}
